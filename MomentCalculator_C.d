04046e8597a0b043beeed0a2896aa481ea38925c
Claude Pruneau <aa7526@wayne.edu>
2023-10-05 13:13:23 -0400

    Initial commit

---
+++ MomentCalculator_C.d
@@ -0,0 +1,48 @@
+
+# DO NOT DELETE
+
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TROOT.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TDirectory.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TNamed.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TObject.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/Rtypes.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/RtypesCore.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ROOT/RConfig.hxx
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/RVersion.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/RConfigure.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/DllImport.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/strtok.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/strlcpy.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/snprintf.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TGenericClassInfo.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TSchemaHelper.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TIsAProxy.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TVirtualIsAProxy.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TStorage.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TVersionCheck.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/RVersion.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TString.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TMathBase.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ROOT/RStringView.hxx
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ROOT/TypeTraits.hxx
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TClass.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TDictionary.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ESTLType.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TObjArray.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TSeqCollection.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TCollection.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TIterator.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TVirtualRWMutex.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TVirtualMutex.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ROOT/RRangeCast.hxx
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ROOT/RSpan.hxx
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/ROOT/span.hxx
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TUUID.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TList.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TBuffer.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/TDataType.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/Bytes.h
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/Byteswap.h
+./MomentCalculator_C.so: PermutationGenerator.C
+./MomentCalculator_C.so: /Users/pruneau/opt/root/include/RVersion.h /Users/pruneau/opt/root/include/ROOT/RConfig.hxx /Users/pruneau/opt/root/include/TClass.h /Users/pruneau/opt/root/include/TDictAttributeMap.h /Users/pruneau/opt/root/include/TInterpreter.h /Users/pruneau/opt/root/include/TROOT.h /Users/pruneau/opt/root/include/TBuffer.h /Users/pruneau/opt/root/include/TMemberInspector.h /Users/pruneau/opt/root/include/TError.h /Users/pruneau/opt/root/include/RtypesImp.h /Users/pruneau/opt/root/include/TIsAProxy.h /Users/pruneau/opt/root/include/TFileMergeInfo.h /Users/pruneau/opt/root/include/TCollectionProxyInfo.h /Users/pruneau/opt/root/bin/rootcling
+MomentCalculator_C__ROOTBUILDVERSION= 6.28/06
---
+++ README.md
@@ -0,0 +1,2 @@
+# MomentsCalculator
+ 
